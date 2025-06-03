d34886d32b2a938767f470259313a10197f95a98
Lucastiru <lucas.12.silva@hotmail.com>
2025-06-03 18:11:18 -0300

    Update simulador.py

--- simulador.py
+++ simulador.py
@@ -3,11 +3,14 @@ def calcular_preco(custo, imposto_pct, embalagem, frete, comissao_pct, margem_pc
     custo_total = custo + imposto + embalagem
     preco_venda = (custo_total + frete) / (1 - (comissao_pct + margem_pct) / 100)
 
-    lucro_liquido = preco_venda - custo_total - frete - (preco_venda * (comissao_pct / 100))
+    comissao_valor = preco_venda * (comissao_pct / 100)
+    custo_da_venda = imposto + embalagem + frete + comissao_valor
+    lucro_liquido = preco_venda - custo_total - frete - comissao_valor
     margem_liquida = (lucro_liquido / preco_venda) * 100
 
     return {
         'preco_venda': round(preco_venda, 2),
         'lucro_liquido': round(lucro_liquido, 2),
-        'margem_liquida': round(margem_liquida, 2)
+        'margem_liquida': round(margem_liquida, 2),
+        'custo_da_venda': round(custo_da_venda, 2)
     }
